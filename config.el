bca0f00a693ce446112d6ae9bbccfb3f21f61a3b
liyang <7325560@qq.com>
2023-10-09 12:02:21 +0800

    update doom version adjust config.

--- config.el
+++ config.el
@@ -15,7 +15,7 @@
 ;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
 ;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
-;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-symbol-font' -- for symbols
 ;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
 ;; See 'C-h v doom-font' for documentation and more examples of what they
@@ -23,26 +23,8 @@
 ;;
 ;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
 ;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
-;;
-(setq doom-font (font-spec :family "Source Code Pro" :size 13 :weight 'normal :width 'normal)
-      doom-variable-pitch-font (font-spec :family "Source Code Pro" :size 13 :weight 'normal :width 'normal)
-      doom-big-font (font-spec :family "Source Code Pro" :size 26 :weight 'normal :width 'normal))
-
-(add-hook! 'after-make-frame-functions #'setup-frame-chinese-font)
-
-(defun setup-frame-chinese-font(frame)
-  (when (display-graphic-p)
-    (dolist (charset '(kana han cjk-misc bopomofo))
-      (set-fontset-font (frame-parameter frame 'font)
-                        charset (font-spec :family "Hiragino Sans GB" :size 16)))))
-
-(defun setup-chinese-font()
-  (interactive)
-  (setup-frame-chinese-font nil))
-  ;; (if (display-graphic-p)
-  ;;     (dolist (charset '(kana han cjk-misc bopomofo))
-  ;;       (set-fontset-font (frame-parameter nil 'font)
-  ;;                         charset (font-spec :family "Hiragino Sans GB" :size 16)))))
+(setq doom-font (font-spec :family "Sarasa Mono SC" :size 14)
+      doom-variable-pitch-font (font-spec :family "Sarasa Fixed SC" :size 14))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -61,23 +43,6 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
-(add-to-list 'auto-mode-alist '("\\.inl\\'" . c-or-c++-mode))
-
-(add-hook! cc-mode
-           (setq-local tab-width 4
-                       c-basic-offset 4))
-
-;; (setq lsp-csharp-server-path "/Users/liyang/bin/omnisharp/omnisharp-osx-x64-net6.0/OmniSharp")
-(setq lsp-csharp-server-path "/Users/liyang/bin/omnisharp/omnisharp-osx/run")
-
-(after! lsp-mode
-  (setq lsp-lens-enable nil)
-  (setq lsp-enable-file-watchers nil))
-
-(after! lsp-ui
-  (setq lsp-ui-doc-enable t)
-  (setq lsp-ui-sideline-enable nil))
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
@@ -111,3 +76,108 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(defadvice! projectile-files-via-ext-command-around (fn root command)
+  :around '(projectile-files-via-ext-command)
+  (when command
+    (let ((cmd command))
+      (let ((unity-ver-file (cond ((locate-file "ProjectVersion.txt"
+                                                `(,(concat (file-name-as-directory root)
+                                                           "ProjectSettings"))))
+                                  ((locate-file "ProjectVersion.txt"
+                                                `(,(concat (file-name-as-directory root)
+                                                           (file-name-as-directory "unity")
+                                                           "ProjectSettings")))))))
+        (if unity-ver-file
+            (setq cmd (concat cmd " -e sh -e cs -e txt -e md -e txt -e json -e xml -e bytes -e lua -e shader -e cginc"))))
+      (message "ext command run at root %s with command %s" root cmd)
+      (funcall fn root cmd))))
+
+(add-to-list 'auto-mode-alist '("cginc\\'" . shader-mode))
+(add-to-list 'auto-mode-alist '("glslinc\\'" . shader-mode))
+
+(use-package! lsp-mode
+  :config
+  (setq lsp-csharp-server-path "/usr/local/bin/omnisharp")
+  (cl-loop for it in '("[/\\\\]Library\\'"
+                       "[/\\\\]Temp\\'"
+                       "[/\\\\]Builds\\'"
+                       "[/\\\\]DownloadAssets\\'"
+                       "[/\\\\]Logs\\'"
+                       "[/\\\\]obj\\'"
+                       "[/\\\\]_Android\\'"
+                       "[/\\\\]_iOS\\'")
+           do (add-to-list 'lsp-file-watch-ignored-directories it))
+
+  (cl-loop for it in '("[/\\\\].+\\.csproj\\'"
+                       "[/\\\\].+\\.sln\\'"
+                       "[/\\\\].+\\.meta\\'")
+           do (add-to-list 'lsp-file-watch-ignored-files it))
+  (setq lsp-file-watch-threshold 2000))
+
+(defun wood9366/project-try-projectile (dir)
+  (let ((probe (locate-dominating-file dir ".projectile")))
+    (when probe (cons 'transient probe))))
+
+(add-hook 'project-find-functions #'wood9366/project-try-projectile 'append)
+
+(add-hook 'c++-mode-hook
+          (lambda()
+            (setq-local flycheck-disabled-checkers '(c/c++-clang c/c++-gcc c/c++-cppcheck))))
+
+
+;; org reveal
+(after! org-re-reveal
+  (setq org-re-reveal-root (concat "file://" (expand-file-name "~/packs/reveal.js"))
+        org-re-reveal-revealjs-version "4"))
+
+;; lua
+(add-to-list 'auto-mode-alist '("lua\\.txt\\'" . lua-mode))
+(add-to-list 'auto-mode-alist '("lua\\.bytes\\'" . lua-mode))
+
+(after! lua-mode
+  (setq lua-indent-level 4))
+
+(add-hook! lua-mode
+  (add-hook! 'xref-backend-functions :local #'etags--xref-backend))
+
+;; perl
+(defalias 'perl-mode 'cperl-mode)
+
+(add-to-list 'auto-mode-alist
+             '("Construct\\'" . perl-mode))
+
+(after! cperl-mode
+  (setq
+   cperl-close-paren-offset -4
+   cperl-continued-statement-offset 4
+   cperl-indent-level 4
+   cperl-indent-parens-as-block t
+   cperl-tabs-always-indent t))
+
+(defun markdown-preview-filter (buffer)
+  (princ (with-current-buffer buffer
+    (format "<!DOCTYPE html><html><title>Impatient Markdown</title><xmp theme=\"united\" style=\"display:none;\"> %s  </xmp><script src=\"http://strapdownjs.com/v/0.2/strapdown.js\"></script></html>" (buffer-substring-no-properties (point-min) (point-max))))
+  (current-buffer)))
+
+(defun markdown-preview-start ()
+  "start preview markdown in browser"
+  (interactive)
+  (if (eq major-mode 'markdown-mode)
+      (progn
+        (unless (httpd-running-p)
+          (httpd-start))
+        (impatient-mode)
+        (imp-set-user-filter #'markdown-preview-filter)
+        (browse-url (format "http://localhost:8080/imp/live/%s/" (buffer-name))))
+    (message "current buffer isn't markdown mode")))
+
+(defun markdown-preview-stop()
+  "stop preview markdown in browser"
+  (interactive)
+  (if (eq major-mode 'markdown-mode)
+      (progn
+        (if (httpd-running-p)
+            (httpd-stop))
+        (impatient-mode 0)
+        (imp-remove-user-filter))))
--- init.el
+++ init.el
@@ -36,17 +36,17 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
-       ;;ligatures         ; ligatures and symbols to make your code pretty again
+       ligatures         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
-       ;;nav-flash         ; blink cursor line after big motions
+       nav-flash         ; blink cursor line after big motions
        ;;neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
        ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
-       ;;unicode           ; extended unicode support for various languages
-       vc-gutter         ; vcs diff in the fringe
+       unicode           ; extended unicode support for various languages
+       (vc-gutter +pretty) ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
@@ -59,7 +59,7 @@
        ;;(format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
-       ;;multiple-cursors  ; editing in many places at once
+       multiple-cursors  ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
@@ -87,12 +87,12 @@
        :tools
        ;;ansible
        ;;biblio            ; Writes a PhD for you (citation needed)
+       ;;collab            ; buffers with friends
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
-       ;;editorconfig      ; let someone else argue about tabs vs spaces
+       editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
-       tree-sitter
        (eval +overlay)     ; run code, run (also, repls)
        ;;gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
@@ -106,6 +106,7 @@
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
+       ;;tree-sitter       ; syntax and parsing, sitting in a tree...
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os
@@ -120,7 +121,7 @@
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
-       (csharp +lsp +unity +tree-sitter)            ; unity, .NET, and mono shenanigans
+       (csharp +unity +lsp); unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;dhall
@@ -142,7 +143,7 @@
        ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
        ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       ;;javascript        ; all(hope(abandon(ye(who(enter(here))))))
+       javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        ;;latex             ; writing papers in Emacs has never been so fun
@@ -153,7 +154,7 @@
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       org               ; organize your plain life in plain text
+       (org +present)      ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
@@ -164,7 +165,7 @@
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
        ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
        sh                ; she sells {ba,z,fi}sh shells on the C xor
